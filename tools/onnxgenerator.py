aec0215858de408d97f521237763beaf8d17d703
FAREAST\chazhan <chazhan@microsoft.com>
2018-09-04 17:44:19 +0800

    adopt to rs5

--- tools/onnxgenerator.py
+++ tools/onnxgenerator.py
@@ -1,18 +1,18 @@
 import os
 from winmltools import convert_coreml
-from winmltools.utils import save_model
-from winmltools.utils import save_text
+from winmltools.utils import save_model, save_text, convert_float_to_float16
 from coremltools.models.utils import load_spec
+import winmltools
 
-print 'CoreML model path:'
-ml_file = str(raw_input())
+print('CoreML model path:')
+ml_file = str(input())
 ml_path = os.path.split(ml_file)
 folder_path = ml_path[0]
 # Load model file
 model_coreml = load_spec(ml_file)
 
-print 'ONNX model name:'
-ml_name = str(raw_input())
+print('ONNX model name:')
+ml_name = str(input())
 
 # Convert it!
 # The automatic code generator (mlgen) uses the name parameter to generate
@@ -20,16 +20,21 @@ ml_name = str(raw_input())
 model_onnx = convert_coreml(model_coreml, name=ml_name)
 onnx_file_name = os.path.join(folder_path, ml_path[1].split('.')[0])
 
+print('Convert to floating point 16? (y/n)')
+need_float_16=input()
+if(need_float_16=='y'):
+    model_onnx=convert_float_to_float16(model_onnx)
+
 # Save the produced ONNX model in binary format
 onnx_path = onnx_file_name + '.onnx'
 save_model(model_onnx, onnx_path)
-print onnx_path
+print(onnx_path)
 
 #save_text(model_onnx, 'example.txt')
 # Save as text
-print "do you want generate json model file? (y/n)"
-need_json = str(raw_input())
+print('do you want generate json model file? (y/n)')
+need_json = str(input())
 if(need_json == 'y'):
-    json_path=onnx_file_name + '.json'
+    json_path = onnx_file_name + '.json'
     save_text(model_onnx, json_path)
-    print json_path
+    print(json_path)
